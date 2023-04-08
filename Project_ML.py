05faa1bdd2d05ced7c85e730b5c80658d0e8891e
RaniaGHARRAD <126870288+RaniaGHARRAD@users.noreply.github.com>
2023-04-09 01:32:15 +0200

    Update Project_ML.py

--- Project_ML.py
+++ Project_ML.py
@@ -24,10 +24,10 @@ from sklearn.ensemble import RandomForestRegressor
 # Load the dataset
 data_train = pd.read_csv('/content/x_train.csv')
 
-# Examine data types
+# Data types (numerical, categorical...?)
 print(data_train.dtypes)
 
-#Check the shape of the training dataset
+#the shape of the training dataset= the number of rows and columns in an array
 print(data_train.shape)
 
 # check for missing values 
@@ -36,7 +36,7 @@ print(data_train.isna().sum())
 # visualize data distribution using histograms or boxplots
 data_train.hist()
 
-# get a list of column names and data types in roder to see which ones to drop 
+# get a list of column names and data types in oder to see which ones to drop 
 col_types = data_train.dtypes
 print (col_types)
 
@@ -74,7 +74,7 @@ X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.
 # Build the model
 model = Sequential()
 model.add(Dense(64, activation='relu', input_dim=X_train.shape[1]))
-model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01)))
 model.add(Dense(y_train.shape[1], activation='softmax'))
 
 # Compile the model
@@ -87,23 +87,13 @@ history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=
 val_loss, val_acc = model.evaluate(X_val, y_val)
 print("Validation loss:", val_loss)
 print("Validation accuracy:", val_acc)
-
-# Get predictions on validation set
-X_val_pred = model.predict(X_val)
-X_val_pred = np.argmax(X_val_pred, axis=1)
-y_val_true = np.argmax(y_val, axis=1)
-
-# Print validation set results
-print("True classes:", y_val_true)
-print("Predicted classes:", X_val_pred)
-print("Validation loss:", val_loss)
-print("Validation accuracy:", val_acc)
+#Validation loss: 0.09820565581321716 ----Validation accuracy: 0.96875
 
 # Evaluate the model on training data
-test_loss, test_acc = model.evaluate(X_train, y_train)
-print("Test loss:", test_loss)
-print("Test accuracy:", test_acc)
-
+train_loss, train_acc = model.evaluate(X_train, y_train)
+print("Test loss:", train_loss)
+print("Test accuracy:", train_acc)
+#Train loss: 0.09903780370950699 ------Train accuracy: 0.9658203125
 
 # Plot the training and validation loss
 plt.figure()
@@ -125,42 +115,61 @@ plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
 plt.show()
-
+#Validation loss: 0.08983644098043442 --Validation accuracy: 0.9697265625
+#Train loss: 0.07767543196678162 ---Train accuracy: 0.970458984375
 
 #######################################################################################################
 # Part 4-Performance tunning
-# Build the model with more batch size  more epochs and regulators and dropouts as well as the early stoppings
-
+# Build the model
 model = Sequential()
 model.add(Dense(64, activation='relu', input_dim=X_train.shape[1]))
-model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01)))
-model.add(Dense(16, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.3))
 model.add(Dense(y_train.shape[1], activation='softmax'))
 
 # Compile the model
-opt = Adam(learning_rate=0.001)
-model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+optimizer = Adam(learning_rate=0.001)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-# Define the EarlyStopping callback
-early_stopping = EarlyStopping(
-    monitor='val_loss',
-    patience=10,
-    verbose=1,
-    mode='min',
-    restore_best_weights=True)
-# Train the model 
-
-history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stopping])
+# Train the model
+history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_val, y_val))
 
 
 # Evaluate the model on validation data
 val_loss, val_acc = model.evaluate(X_val, y_val)
 print("Validation loss:", val_loss)
 print("Validation accuracy:", val_acc)
-#Validation loss: 0.10264364629983902
-#Validation accuracy: 0.9684244990348816
 
+#Validation loss: 0.0855894684791565---Validation accuracy: 0.9723307490348816
+
+# Evaluate the model on training data
+train_loss, train_acc = model.evaluate(X_train, y_train)
+print("Train loss:", train_loss)
+print("Train accuracy:", train_acc)
+
+#Train loss: 0.07513316720724106---Train accuracy: 0.97412109375
+
+# Plot the training and validation loss for tunning part
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Training and validation loss-tunning')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# Plot the training and validation accuracy
+
+plt.figure()
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Training and validation accuracy-tunning')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
 #######################################################################################################
  # Partie 5-Model evaluation - check how well the model performs on the testing dataset
 
@@ -205,15 +214,17 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 # Train the model
 history = model.fit(X_test, y_test, epochs=50, batch_size=32, validation_data=(X_val, y_val))
 
-# Evaluate the model on validation data
+# Evaluate the model on the validation data
 val_loss, val_acc = model.evaluate(X_val, y_val)
 print("Validation loss:", val_loss)
 print("Validation accuracy:", val_acc)
+#Validation loss: 0.15977898240089417 ----Validation accuracy: 0.95703125
 
-# Evaluate the model on testing data
+# Evaluate the model on the testing data
 test_loss, test_acc = model.evaluate(X_test, y_test)
 print("Test loss:", test_loss)
 print("Test accuracy:", test_acc)
+#Test loss: 0.09366018325090408 ---Test accuracy: 0.9736328125
 
 ##Plot the testing and validation loss
 plt.figure()
@@ -236,9 +247,6 @@ plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
-#the result i got
-#Validation loss: 0.1556047946214676 ---Validation accuracy: 0.9544270634651184
-#Test loss: 0.09221438318490982  -----Test accuracy: 0.9733073115348816
 # Get predictions on testing set
 X_test_pred = model.predict(data_test.iloc[:, :-1])
 X_test_pred = X_test_pred.reshape(-1, y_test.shape[1])
@@ -259,7 +267,7 @@ result_test.to_csv('test_result.csv', index=False)
 #MLP training time: 24.474051475524902
 #MLP accuracy: 0.96875
 
-##Loading my traning dataset and doing the same processing
+# Load the dataset
 data_train = pd.read_csv('/content/x_train.csv')
 
 # Drop the columns which are not for use
@@ -275,6 +283,10 @@ data_train[num_cols] = scaler.fit_transform(data_train[num_cols])
 X_train = data_train.iloc[:, :-1].values
 y_train = data_train.iloc[:, -1].values.astype(int)
 
+# Convert target labels to binary encoded format
+# Convert target labels to binary encoded format
+lb = LabelBinarizer()
+y_test = lb.fit_transform(y_test)
 # Split the dataset into training and validation sets
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=0)
 
@@ -291,28 +303,49 @@ y_val_pred = svm.predict(X_val)
 val_acc = accuracy_score(y_val, y_val_pred)
 
 # Calculate training time
-training_time = end_time - start_time
+training_time_svm = end_time - start_time
 
 # Print SVM results
 print("SVM validation accuracy:", val_acc)
-print("SVM training time:", training_time)
+print("SVM training time:", training_time_svm)
 
 # Evaluate the SVM model on the validation set
 x_pred_svm = svm.predict(X_val)
 svm_accuracy = accuracy_score(y_val, x_pred_svm)
 print("SVM accuracy:", svm_accuracy)
 
-# Train an MLP model with the same architecture and hyperparameters as in your code
+# Train an MLP model with the same architecture and hyperparameters as in my code pfpart 3
 mlp = MLPClassifier(hidden_layer_sizes=(64,32), activation='relu', solver='adam', alpha=0.01, batch_size=32, max_iter=500, validation_fraction=0.2)
 start_time = time.time()
 mlp.fit(X_train, y_train)
-print("MLP training time:", time.time()-start_time)
+end_time = time.time()
+# Calculate training time
+training_time_MLP = end_time - start_time
+print("MLP training time:", training_time_MLP)
 
 # Evaluate the MLP model on the validation set
 x_pred_mlp = mlp.predict(X_val)
 mlp_accuracy = accuracy_score(y_val, x_pred_mlp)
 print("MLP accuracy:", mlp_accuracy)
+import matplotlib.pyplot as plt
 
+# Set the x-axis labels for the plots
+models = ['SVM', 'MLP']
+
+# Set the y-axis values for the plots
+val_acc = [svm_accuracy, mlp_accuracy]
+train_time = [training_time_svm, training_time_MLP]
+
+# Plot the validation accuracy
+plt.bar(models, val_acc)
+plt.ylim([0.7, 1])
+plt.title('Validation Accuracy')
+plt.show()
+
+# Plot the training time
+plt.bar(models, train_time)
+plt.title('Training Time')
+plt.show()
 
 
 
@@ -347,7 +380,7 @@ clf.fit(X_train, y_train)
 sfm = SelectFromModel(clf, threshold=0.1)
 
 # Experiment with different number of features
-n_features = [2, 3, 5, 7]
+n_features = [1,2, 3,4,5,6, 7,8]
 val_list = []
 
 for n in n_features:
@@ -382,3 +415,4 @@ plt.show()
 #Number of features: 3 Validation MSE: 0.06318943947553635
 #Number of features: 5 Validation MSE: 0.060656312853097916
 #Number of features: 7 Validation MSE: 0.062497127801179886
+
